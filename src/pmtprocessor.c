43ce11394779396faa2343dfbe4f9c43e809af45
Adam Charrett <adam@dvbstreamer.org>
2006-04-18 19:52:43 +0000

    Update copyright statements and documentation

--- src/pmtprocessor.c
+++ src/pmtprocessor.c
@@ -1,10 +1,29 @@
+/* 
+Copyright (C) 2006  Adam Charrett
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+pmtprocessor.c
+
+Process Program Map Tables and update the services information and PIDs.
+
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <linux/types.h>
-typedef unsigned char   uint8_t;
-typedef unsigned short  uint16_t;
-typedef unsigned int    uint32_t;
+#include <stdint.h>
 #include <dvbpsi/dvbpsi.h>
 #include <dvbpsi/descriptor.h>
 #include <dvbpsi/dr.h>
