7b38674ef0e1101e1022fe70ac70f03f9c147082
Adam Charrett <adam@dvbstreamer.org>
2006-06-23 13:44:14 +0000

    Add IPv6 Support

--- include/udp.h
+++ include/udp.h
@@ -15,21 +15,26 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  
-udpsend.h
+udp.h
  
 Simplify UDP socket creation and packet sending.
  
 */
-#ifndef _UDPSEND_H
-#define _UDPSEND_H
+#ifndef _UDP_H
+#define _UDP_H
 #include <netinet/in.h>
 
 #define MTU 1400 /* Conservative estimate */
 #define IP_HEADER (5*4)
 #define UDP_HEADER (2*4)
 
-int UDPCreateSocket(struct sockaddr_in *sockaddr, int reuse);
-int UDPSetupSocketAddress(char *host, int port, struct sockaddr_in *sockaddr);
-int UDPSendTo(int socket, char *data, int len, struct sockaddr_in *to);
-int UDPReceiveFrom(int socketfd, char *data, int *len, struct sockaddr_in *from);
+int UDPCreateSocket(sa_family_t family);
+
+#define UDPSendTo(_socketfd, _data, _data_len, _to, _to_len) \
+    sendto(_socketfd, _data, _data_len, 0, _to, _to_len)
+
+
+#define UDPReceiveFrom(_socketfd, _data, _data_len, _from, _from_len) \
+    recvfrom(_socketfd, _data, _data_len, 0, _from, _from_len)
+
 #endif
