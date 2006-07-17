aaa312d0becaebc2d1aaa3d33b4ce597ce1469b8
Adam Charrett <adam@dvbstreamer.org>
2006-07-17 15:59:48 +0000

    Update the python implementation to use the autogenerated msgcodes.py

--- test/binarycomms.py
+++ test/binarycomms.py
@@ -1,60 +1,38 @@
+# Copyright (C) 2006  Adam Charrett
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+#
+# binarycomms.py
+#
+# Binary Communications protocol message implementation.
+#
 import socket
 import struct
 
+# Message Codes
+from msgcodes import *
 
 # Low level data formats
 FORMAT_UINT8  = 'B'
 FORMAT_UINT16 = '>H'
 FORMAT_UINT32 = '>I'
-FORMAT_HEADER = '>BH'
+FORMAT_HEADER = '>HH'
 
-# Base port the DVBStreamer server run on ( + adpter number)
+# Base port the DVBStreamer server run on ( + adapter number)
 PORT = 54197
 
-# Message Codes
-MSGCODE_RERR = 0xFF # = 0xFF = RERR = Response Error   
-MSGCODE_INFO = 0x00 # = 0x00 = INFO = Information  
-MSGCODE_AUTH = 0x01 # = 0x01 = AUTH = Authenticate  
-MSGCODE_CSPS = 0x11 # = 0x11 = CSPS = Control Service Primary Select - Select Primary Service  
-MSGCODE_CSSA = 0x12 # = 0x12 = CSSA = Control Service Secondary Add - Add secondary service  
-MSGCODE_CSSS = 0x13 # = 0x12 = CSSA = Control Service Secondary Select - Select service to stream  
-MSGCODE_CSSR = 0x14 # = 0x13 = CSSR = Control Service Secondary Remove - Remove secondary service  
-MSGCODE_COAO = 0x15 # = 0x14 = COAO = Control Output Add Output - Add a new output destination  
-MSGCODE_CORO = 0x16 # = 0x15 = CORO = Control Output Remove Output - Remove an output destination  
-MSGCODE_COAP = 0x17 # = 0x16 = COAP = Control Output Add PIDs - Add pids to an output.  
-MSGCODE_CORP = 0x18 # = 0x17 = CORP = Control Output remove PIDs - Remove pids from an output.  
-MSGCODE_SSPC = 0x21 # = 0x21 = SSPC = Status Service Primary Current - Return current service name for primary output.  
-MSGCODE_SSSL = 0x22 # = 0x22 = SSSL = Status Service Secondary List - List secondary outputs.  
-MSGCODE_SOLO = 0x23 # = 0x23 = SOLO = Status Outputs List outputs  
-MSGCODE_SOLP = 0x24 # = 0x24 = SOLP = Status Output List pids  
-MSGCODE_SOPC = 0x25 # = 0x25 = SOPC = Status Output Packet Count  
-MSGCODE_STSS = 0x26 # = 0x26 = STSS = Status TS Stats  
-MSGCODE_SFES = 0x27 # = 0x27 = SFES = Status Front End Status  
-MSGCODE_SSLA = 0x28 # = 0x28 = SSLA = Status Services List All - List all avaialable services  
-MSGCODE_SSLM = 0x29 # = 0x29 = SSLM = Status Services List Multiplex - List services avaialable of the current multiplex  
-MSGCODE_SSPL = 0x2A # = 0x2A = SSPL = Status Services List PIDs  
-MSGCODE_RSSL = 0x31 # = 0x31 = RSSL = Response Service Secondary List  
-MSGCODE_ROLO = 0x32 # = 0x32 = ROLO = Response Outputs List outputs  
-MSGCODE_RLP  = 0x33 # = 0x33 = RLP  = Response Output List pids  
-MSGCODE_ROPC = 0x34 # = 0x34 = ROPC = Response Output Packet Count  
-MSGCODE_RTSS = 0x35 # = 0x35 = RTSS = Response TS Stats  
-MSGCODE_RFES = 0x36 # = 0x36 = RFES = Response Front End Status  
-MSGCODE_RSL  = 0x37 # = 0x37 = RSL  = Response Services List  
-
-# Error Codes
-RERR_OK               = 0x00
-RERR_NOTAUTHORISED    = 0x01
-RERR_ALREADYEXISTS    = 0x02
-RERR_NOSUCHOUTPUT     = 0x03
-RERR_ALREADYSTREAMING = 0x04
-RERR_UNDEFINED        = 0xff
-
-# Info codes
-INFO_NAME             = 0x00
-INFO_FETYPE           = 0x01
-INFO_AUTHENTICATED    = 0x02
-INFO_UPTIMESEC        = 0xFE
-INFO_UPTIME           = 0xFF
 
 
 def MessageReadString(body):
@@ -95,30 +73,30 @@ def MessageSend(socket, type, body):
     socket.send(header + body)
 
 def MessageRecv(socket):
-    header = socket.recv(3)
+    header = socket.recv(struct.calcsize(FORMAT_HEADER))
     (type, length) =  struct.unpack(FORMAT_HEADER, header)
     body = socket.recv(length)
     
     return (type, body)
 
 def MessageDecode(type, body):
-    if type == MSGCODE_RERR : # = 0xFF = RERR = Response Error  
+    if type == MSGCODE_RERR : # Response Error  
         (code, body) = MessageReadUInt8(body)
         (str,  body) = MessageReadString(body)
         return (code, str)
-    if type == MSGCODE_RSSL : # = 0x31 = RSSL = Response Service Secondary List  
+    if type == MSGCODE_RSSL : # Response Service Secondary List  
         pass
-    if type == MSGCODE_ROLO : # = 0x32 = ROLO = Response Outputs List outputs  
+    if type == MSGCODE_ROLO : # Response Outputs List outputs  
         pass
-    if type == MSGCODE_RLP  : # = 0x33 = RLP  = Response Output List pids  
+    if type == MSGCODE_RLP  : # Response Output List pids  
         pass
-    if type == MSGCODE_ROPC : # = 0x34 = ROPC = Response Output Packet Count  
+    if type == MSGCODE_ROPC : # Response Output Packet Count  
         pass
-    if type == MSGCODE_RTSS : # = 0x35 = RTSS = Response TS Stats  
+    if type == MSGCODE_RTSS : # Response TS Stats  
         pass
-    if type == MSGCODE_RFES : # = 0x36 = RFES = Response Front End Status  
+    if type == MSGCODE_RFES : # Response Front End Status  
         pass
-    if type == MSGCODE_RSL  : # = 0x37 = RSL  = Response Services List      
+    if type == MSGCODE_RLS  : # Response Services List      
         (nrofservices, body) = MessageReadUInt16(body)
         i = 0
         services = []
@@ -139,26 +117,26 @@ def MessageEncode(type, *args):
     """
     body = ''
     
-    if type == MSGCODE_INFO : # = 0x00 = INFO = Information  
+    if type == MSGCODE_INFO : # Information  
         body = MessageWriteUInt8(body, args[0])
         
-    elif ((type == MSGCODE_AUTH) or # = 0x01 = AUTH = Authenticate  
-         (type == MSGCODE_CSSA) or # = 0x12 = CSSA = Control Service Secondary Add - Add secondary service  
-         (type == MSGCODE_CSSS) or # = 0x12 = CSSA = Control Service Secondary Select - Select service to stream  
-         (type == MSGCODE_COAO)):   # = 0x14 = COAO = Control Output Add Output - Add a new output destination  
+    elif ((type == MSGCODE_AUTH) or # Authenticate  
+         (type == MSGCODE_CSSA) or # Control Service Secondary Add - Add secondary service  
+         (type == MSGCODE_CSSS) or # Control Service Secondary Select - Select service to stream  
+         (type == MSGCODE_COAO)):   # Control Output Add Output - Add a new output destination  
         body = MessageWriteString(body, args[0]) # Username, Service Output name, Service Output name, Output name
         body = MessageWriteString(body, args[1]) # Password, IP:PORT, Service Name, IP: Port
         
-    elif ((type == MSGCODE_CSPS) or # = 0x11 = CSPS = Control Service Primary Select - Select Primary Service  
-         (type == MSGCODE_CSSR) or # = 0x13 = CSSR = Control Service Secondary Remove - Remove secondary service  
-         (type == MSGCODE_CORO) or # = 0x15 = CORO = Control Output Remove Output - Remove an output destination 
-         (type == MSGCODE_SOLP) or # = 0x24 = SOLP = Status Output List pids 
-         (type == MSGCODE_SOPC) or # = 0x25 = SOPC = Status Output Packet Count 
-         (type == MSGCODE_SSPL)) : # = 0x2A = SSPL = Status Services List PIDs  
+    elif ((type == MSGCODE_CSPS) or # Control Service Primary Select - Select Primary Service  
+         (type == MSGCODE_CSSR) or # Control Service Secondary Remove - Remove secondary service  
+         (type == MSGCODE_CORO) or # Control Output Remove Output - Remove an output destination 
+         (type == MSGCODE_SOLP) or # Status Output List pids 
+         (type == MSGCODE_SOPC) or # Status Output Packet Count 
+         (type == MSGCODE_SSPL)) : # Status Services List PIDs  
         body = MessageWriteString(body, args[0]) # Service name,  Service Output name, Output name
 
-    elif ((type == MSGCODE_COAP) or # = 0x16 = COAP = Control Output Add PIDs - Add pids to an output.  
-         (type == MSGCODE_CORP)) :  # = 0x17 = CORP = Control Output remove PIDs - Remove pids from an output.  
+    elif ((type == MSGCODE_COAP) or # Control Output Add PIDs - Add pids to an output.  
+         (type == MSGCODE_CORP)) :  # Control Output remove PIDs - Remove pids from an output.  
         body = MessageWriteString(body, args[0]) # Output name
         pids = arg[1]
         nrofpids = len(pids)
@@ -171,13 +149,13 @@ def MessageEncode(type, *args):
         for pid in pids:
             body = MessageWriteUInt16(body, pid)
 
-    elif ((type == MSGCODE_SSPC) or # = 0x21 = SSPC = Status Service Primary Current - Return current service name for primary output.  
-         (type == MSGCODE_SSSL) or # = 0x22 = SSSL = Status Service Secondary List - List secondary outputs.  
-         (type == MSGCODE_SOLO) or # = 0x23 = SOLO = Status Outputs List outputs  
-         (type == MSGCODE_STSS) or # = 0x26 = STSS = Status TS Stats  
-         (type == MSGCODE_SFES) or # = 0x27 = SFES = Status Front End Status  
-         (type == MSGCODE_SSLA) or # = 0x28 = SSLA = Status Services List All - List all avaialable services  
-         (type == MSGCODE_SSLM)) : # = 0x29 = SSLM = Status Services List Multiplex - List services avaialable of the current multiplex  
+    elif ((type == MSGCODE_SSPC) or # Status Service Primary Current - Return current service name for primary output.  
+         (type == MSGCODE_SSSL) or # Status Service Secondary List - List secondary outputs.  
+         (type == MSGCODE_SOLO) or # Status Outputs List outputs  
+         (type == MSGCODE_STSS) or # Status TS Stats  
+         (type == MSGCODE_SFES) or # Status Front End Status  
+         (type == MSGCODE_SSLA) or # Status Services List All - List all avaialable services  
+         (type == MSGCODE_SSLM)) : # Status Services List Multiplex - List services avaialable of the current multiplex  
         pass
     else:
         print "Unknown message type to encode %d!\n" % type
@@ -196,16 +174,16 @@ def test():
     length = len(body)
     (code, str) = MessageDecode(type, body)
     
-    print "Type %d (length %d) Code %d String \'%s\'" % (type, length, code, str)
+    print "Type 0x%04x (length %d) Code %d String \'%s\'" % (type, length, code, str)
     
-    body = MessageEncode(MSGCODE_AUTH, "adam", "test")
+    body = MessageEncode(MSGCODE_AUTH, "dvbstreamer", "control")
     MessageSend(s, MSGCODE_AUTH, body )
     
     (type, body) = MessageRecv(s)
     length = len(body)
     (code, str) = MessageDecode(type, body)
     
-    print "Type %d (length %d) Code %d String \'%s\'" % (type, length, code, str)
+    print "Type 0x%04x (length %d) Code %d String \'%s\'" % (type, length, code, str)
     
     
     body = MessageEncode(MSGCODE_INFO, INFO_AUTHENTICATED)
@@ -215,7 +193,7 @@ def test():
     length = len(body)
     (code, str) = MessageDecode(type, body)
     
-    print "Type %d (length %d) Code %d String \'%s\'" % (type, length, code, str)
+    print "Type 0x%04x (length %d) Code %d String \'%s\'" % (type, length, code, str)
     
     body = MessageEncode(MSGCODE_AUTH, "adam", "t1est")
     MessageSend(s, MSGCODE_AUTH, body )
@@ -224,7 +202,7 @@ def test():
     length = len(body)
     (code, str) = MessageDecode(type, body)
     
-    print "Type %d (length %d) Code %d String \'%s\'" % (type, length, code, str)
+    print "Type 0x%04x (length %d) Code %d String \'%s\'" % (type, length, code, str)
     
     
     body = MessageEncode(MSGCODE_INFO, INFO_AUTHENTICATED)
@@ -234,7 +212,7 @@ def test():
     length = len(body)
     (code, str) = MessageDecode(type, body)
     
-    print "Type %d (length %d) Code %d String \'%s\'" % (type, length, code, str)
+    print "Type 0x%04x (length %d) Code %d String \'%s\'" % (type, length, code, str)
     
     
     MessageSend(s, MSGCODE_SSLA, '' )
