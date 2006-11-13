119e0a6ad074c32a1a69b3346cb1a43401721116
charrea6 <charrea6@6517040a-6511-0410-b124-e4cd9a02cb2e>
2006-11-13 12:58:57 +0000

    use vfprintf instead of vasprintf

--- trunk/src/logging.c
+++ trunk/src/logging.c
@@ -1,24 +1,24 @@
 /*
 Copyright (C) 2006  Adam Charrett
- 
+
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
- 
+
 logging.h
- 
+
 Logging functions.
- 
+
 */
 #define _GNU_SOURCE
 #include <stdlib.h>
@@ -40,8 +40,8 @@ void printlog(int level, char *format, ...)
     {
         va_list valist;
         char *logline;
-        va_start(valist, format);
-        vasprintf(&logline, format, valist);
+        int len;
+
         if (DaemonMode)
         {
             char buffer[24]; /* "YYYY-MM-DD HH:MM:SS" */
@@ -55,12 +55,13 @@ void printlog(int level, char *format, ...)
             strftime (buffer, sizeof(buffer), "%F %T : ", loctime);
             fputs(buffer, stderr);
         }
-        fputs(logline, stderr);
+
+        va_start(valist, format);
+        vfprintf(stderr, format, valist);
         if (DaemonMode)
         {
             fflush(stderr);
         }
         va_end(valist);
-        free(logline);
     }
 }
