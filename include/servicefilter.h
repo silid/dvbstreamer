5af7b3640c288581f46ab86a5f8158579acf59dd
Adam Charrett <adam@dvbstreamer.org>
2006-11-21 17:10:02 +0000

    Add AVS Only option to the service filter to only stream the main video/audio/subtitles

--- include/servicefilter.h
+++ include/servicefilter.h
@@ -1,22 +1,22 @@
 /*
 Copyright (C) 2006  Adam Charrett
- 
+
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
- 
+
 servicefilter.h
- 
+
 Filter all packets for a service include the PMT, rewriting the PAT sent out in
 the output to only include this service.
 
@@ -31,4 +31,6 @@ void ServiceFilterDestroy(PIDFilter_t *filter);
 void ServiceFilterServiceSet(PIDFilter_t *filter, Service_t *service);
 Service_t *ServiceFilterServiceGet(PIDFilter_t *filter);
 
+void ServiceFilterAVSOnlySet(PIDFilter_t *filter, bool enable);
+bool ServiceFilterAVSOnlyGet(PIDFilter_t *filter);
 #endif
