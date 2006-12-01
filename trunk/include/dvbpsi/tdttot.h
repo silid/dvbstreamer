b2c75258e74ed7b68932ffb5c306c6f92f22e45f
charrea6 <charrea6@6517040a-6511-0410-b124-e4cd9a02cb2e>
2006-12-01 14:24:05 +0000

    Add NIT and TDT/TOT processing functions.

---
+++ trunk/include/dvbpsi/tdttot.h
@@ -0,0 +1,176 @@
+/*
+Copyright (C) 2006  Adam Charrett
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+tdttot.h
+
+Time Date Table and Time Offset Table.
+
+*/
+#ifndef _TDTTOT_H
+#define _TDTTOT_H
+
+/*****************************************************************************
+ * dvbpsi_tdt_t
+ *****************************************************************************/
+/*!
+ * \struct dvbpsi_tdt_tot_t
+ * \brief TDT/TOT structure.
+ *
+ * This structure is used to store a decoded TDT/TOT.
+ * (ETSI EN 300 468 V1.4.1 section 5.2.5/6).
+ */
+/*!
+ * \typedef struct dvbpsi_tdt_tot_s dvbpsi_tdt_tot_t
+ * \brief dvbpsi_tdt_tot_t type definition.
+ */
+typedef struct dvbpsi_tdt_tot_s
+{
+    int                     i_year;  /*!< Year in UTC */
+    int                     i_month; /*!< Month in UTC */
+    int                     i_day;   /*!< Day in UTC */
+    int                     i_hour;  /*!< Hour in UTC */
+    int                     i_minute;/*!< Minute in UTC */
+    int                     i_second;/*!< Second in UTC */
+    dvbpsi_descriptor_t*    p_first_descriptor; /*!< TOT descriptors, only present if the table was a TOT */
+} dvbpsi_tdt_tot_t;
+
+
+/*****************************************************************************
+ * dvbpsi_tdt_tot_callback
+ *****************************************************************************/
+/*!
+ * \typedef void (* dvbpsi_tdt_tot_callback)(void* p_cb_data,
+                                         dvbpsi_tdt_tot_t* p_new_tdt_tot)
+ * \brief Callback type definition.
+ */
+typedef void (* dvbpsi_tdt_tot_callback)(void* p_cb_data, dvbpsi_tdt_tot_t* p_new_tdt_tot);
+
+
+
+/*****************************************************************************
+ * dvbpsi_AttachTDTTOT
+ *****************************************************************************/
+/*!
+ * \fn void dvbpsi_AttachTDTTOT(dvbpsi_tdt_tot_callback pf_callback,
+                               void* p_cb_data)
+ * \brief Creation and initialization of a TDT/TOT decoder.
+ * \param pf_callback function to call back on new NIT.
+ * \param p_cb_data private data given in argument to the callback.
+ * \return a pointer to the decoder for future calls.
+ */
+dvbpsi_handle dvbpsi_AttachTDTTOT(dvbpsi_tdt_tot_callback pf_callback, void* p_cb_data);
+
+
+/*****************************************************************************
+ * dvbpsi_DetachTDTTOT
+ *****************************************************************************/
+/*!
+ * \fn void dvbpsi_DetachTDTTOT(dvbpsi_handle h_dvbpsi)
+ * \brief Destroy a TDT/TOT decoder.
+ * \param h_dvbpsi handle to the decoder
+ * \return nothing.
+ *
+ * The handle isn't valid any more.
+ */
+void dvbpsi_DetachTDTTOT(dvbpsi_handle h_dvbpsi);
+
+
+/*****************************************************************************
+ * dvbpsi_InitTDTTOT/dvbpsi_NewTDTTOT
+ *****************************************************************************/
+/*!
+ * \fn void dvbpsi_InitTDTTOT(dvbpsi_nit_t* p_nit, uint16_t i_network_id,
+          uint8_t i_version, int b_current_next)
+ * \brief Initialize a user-allocated dvbpsi_tdt_tot_t structure.
+ * \param p_tdt_tot pointer to the TDT/TOT structure
+ * \param i_year Year in UTC
+ * \param i_month Month in UTC
+ * \param i_day Day in UTC
+ * \param i_hour Hour in UTC
+ * \param i_minute Minute in UTC
+ * \param i_second Second in UTC
+ * \return nothing.
+ */
+void dvbpsi_InitTDTTOT(dvbpsi_tdt_tot_t *p_tdt_tot,
+                        int i_year, int i_month, int i_day,
+                        int i_hour, int i_minute, int i_second);
+
+/*!
+ * \def dvbpsi_NewTDTTOT(p_nit, i_network_id, i_version, b_current_next)
+ * \brief Allocate and initialize a new dvbpsi_nit_t structure.
+ * \param p_tdt_tot pointer to the TDT/TOT structure
+ * \param i_year Year in UTC
+ * \param i_month Month in UTC
+ * \param i_day Day in UTC
+ * \param i_hour Hour in UTC
+ * \param i_minute Minute in UTC
+ * \param i_second Second in UTC
+ * \return nothing.
+ */
+#define dvbpsi_NewTDTTOT(p_tdt_tot, i_year, i_month, i_day,         \
+                         i_hour, i_minute, i_second)                \
+do {                                                                \
+  p_tdt_tot = (dvbpsi_tdt_tot_t*)malloc(sizeof(dvbpsi_tdt_tot_t));  \
+  if(p_tdt_tot != NULL)                                             \
+    dvbpsi_InitTDTTOT(p_tdt_tot, i_year, i_month, i_day,            \
+                      i_hour, i_minute, i_second);                  \
+} while(0);
+
+
+/*****************************************************************************
+ * dvbpsi_EmptyTDTTOT/dvbpsi_DeleteTDTTOT
+ *****************************************************************************/
+/*!
+ * \fn void dvbpsi_EmptyTDTTOT(dvbpsi_tdt_tot_t* p_tdt_tot)
+ * \brief Clean a dvbpsi_tdt_tot_t structure.
+ * \param p_tdt_tot pointer to the TDT/TOT structure
+ * \return nothing.
+ */
+void dvbpsi_EmptyTDTTOT(dvbpsi_tdt_tot_t *p_tdt_tot);
+
+/*!
+ * \def dvbpsi_DeleteTDTTOT(p_nit)
+ * \brief Clean and free a dvbpsi_nit_t structure.
+ * \param p_nit pointer to the NIT structure
+ * \return nothing.
+ */
+#define dvbpsi_DeleteTDTTOT(p_tdt_tot)                                  \
+do {                                                                    \
+  dvbpsi_EmptyTDTTOT(p_tdt_tot);                                        \
+  free(p_tdt_tot);                                                      \
+} while(0);
+
+/*****************************************************************************
+ * dvbpsi_TOTAddDescriptor
+ *****************************************************************************/
+/*!
+ * \fn dvbpsi_descriptor_t* dvbpsi_TOTAddDescriptor(vbpsi_tdt_tot_t *p_tot,
+                                                    uint8_t i_tag,
+                                                    uint8_t i_length,
+                                                    uint8_t* p_data)
+ * \brief Add a descriptor in the TOT.
+ * \param p_tot pointer to the TOT structure
+ * \param i_tag descriptor's tag
+ * \param i_length descriptor's length
+ * \param p_data descriptor's data
+ * \return a pointer to the added descriptor.
+ */
+dvbpsi_descriptor_t* dvbpsi_TOTAddDescriptor(dvbpsi_tdt_tot_t *p_tot,
+                                             uint8_t i_tag, uint8_t i_length,
+                                             uint8_t* p_data);
+
+#endif
---
+++ trunk/src/dvbpsi/tdttot.c
@@ -0,0 +1,283 @@
+/*
+Copyright (C) 2006  Adam Charrett
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+tdttot.c
+
+Decode Time Date Table and Time Offset Table.
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <dvbpsi/dvbpsi.h>
+#include <dvbpsi/psi.h>
+#include <dvbpsi/descriptor.h>
+#include "dvbpsi/tdttot.h"
+#include "logging.h"
+/*****************************************************************************
+ * dvbpsi_tdt_tot_decoder_s
+ *****************************************************************************
+ * TDT/TOT decoder.
+ *****************************************************************************/
+typedef struct dvbpsi_tdt_tot_decoder_s
+{
+  dvbpsi_tdt_tot_callback       pf_callback;
+  void *                        p_cb_data;
+} dvbpsi_tdt_tot_decoder_t;
+
+/*****************************************************************************
+ * dvbpsi_GatherPATSections
+ *****************************************************************************
+ * Callback for the PSI decoder.
+ *****************************************************************************/
+void dvbpsi_GatherTDTTOTSections(dvbpsi_decoder_t* p_decoder,
+                              dvbpsi_psi_section_t* p_section);
+/*****************************************************************************
+ * dvbpsi_DecodeTDTSection
+ *****************************************************************************
+ * TDT decoder.
+ *****************************************************************************/
+void dvbpsi_DecodeTDTSection(dvbpsi_tdt_tot_t* p_tdt,
+                             dvbpsi_psi_section_t* p_section);
+/*****************************************************************************
+ * dvbpsi_DecodeTOTSection
+ *****************************************************************************
+ * TOT decoder.
+ *****************************************************************************/
+void dvbpsi_DecodeTOTSection(dvbpsi_tdt_tot_t* p_tot,
+                             dvbpsi_psi_section_t* p_section);
+
+void dvbpsi_DecodeMJDUTC(char *p_mjdutc, dvbpsi_tdt_tot_t* p_table);
+
+/*****************************************************************************
+ * dvbpsi_AttachTDTTOT
+ *****************************************************************************
+ * Initialize a TDT/TOT decoder and return a handle on it.
+ *****************************************************************************/
+dvbpsi_handle dvbpsi_AttachTDTTOT(dvbpsi_tdt_tot_callback pf_callback,
+                                  void* p_cb_data)
+{
+  dvbpsi_handle h_dvbpsi = (dvbpsi_decoder_t*)malloc(sizeof(dvbpsi_decoder_t));
+  dvbpsi_tdt_tot_decoder_t* p_tdt_tot_decoder;
+  unsigned int i;
+
+  if(h_dvbpsi == NULL)
+    return NULL;
+
+  p_tdt_tot_decoder = (dvbpsi_tdt_tot_decoder_t*)malloc(sizeof(dvbpsi_tdt_tot_decoder_t));
+
+  if(p_tdt_tot_decoder == NULL)
+  {
+    free(h_dvbpsi);
+    return NULL;
+  }
+
+  /* PSI decoder configuration */
+  h_dvbpsi->pf_callback = &dvbpsi_GatherTDTTOTSections;
+  h_dvbpsi->p_private_decoder = p_tdt_tot_decoder;
+  h_dvbpsi->i_section_max_size = 1024;
+  /* PSI decoder initial state */
+  h_dvbpsi->i_continuity_counter = 31;
+  h_dvbpsi->b_discontinuity = 1;
+  h_dvbpsi->p_current_section = NULL;
+
+  /* TDT/TOT decoder information */
+  p_tdt_tot_decoder->pf_callback = pf_callback;
+  p_tdt_tot_decoder->p_cb_data = p_cb_data;
+
+  return h_dvbpsi;
+}
+
+
+/*****************************************************************************
+ * dvbpsi_DetachTDTTOT
+ *****************************************************************************
+ * Close a TDT/TOT decoder. The handle isn't valid any more.
+ *****************************************************************************/
+void dvbpsi_DetachTDTTOT(dvbpsi_handle h_dvbpsi)
+{
+  free(h_dvbpsi->p_private_decoder);
+  free(h_dvbpsi);
+}
+
+
+/*****************************************************************************
+ * dvbpsi_InitTDTTOT
+ *****************************************************************************
+ * Initialize a pre-allocated dvbpsi_tdt_tot_t structure.
+ *****************************************************************************/
+void dvbpsi_InitTDTTOT(dvbpsi_tdt_tot_t *p_tdt_tot,
+                        int i_year, int i_month, int i_day,
+                        int i_hour, int i_minute, int i_second)
+{
+  p_tdt_tot->i_year = i_year;
+  p_tdt_tot->i_month = i_month;
+  p_tdt_tot->i_day = i_day;
+  p_tdt_tot->i_hour = i_hour;
+  p_tdt_tot->i_minute = i_minute;
+  p_tdt_tot->i_second = i_second;
+  p_tdt_tot->p_first_descriptor = NULL;
+}
+
+/*****************************************************************************
+ * dvbpsi_EmptyTDTTOT
+ *****************************************************************************
+ * Clean a dvbpsi_tdt_tot_t structure.
+ *****************************************************************************/
+void dvbpsi_EmptyTDTTOT(dvbpsi_tdt_tot_t *p_tdt_tot)
+{
+    dvbpsi_DeleteDescriptors(p_tdt_tot->p_first_descriptor);
+}
+
+/*****************************************************************************
+ * dvbpsi_GatherPATSections
+ *****************************************************************************
+ * Callback for the PSI decoder.
+ *****************************************************************************/
+void dvbpsi_GatherTDTTOTSections(dvbpsi_decoder_t* p_decoder,
+                              dvbpsi_psi_section_t* p_section)
+{
+  dvbpsi_tdt_tot_decoder_t* p_tdt_tot_decoder
+                        = (dvbpsi_tdt_tot_decoder_t*)p_decoder->p_private_decoder;
+
+  if(p_section->i_table_id == 0x70)
+  {
+    dvbpsi_tdt_tot_t* p_tdt;
+    dvbpsi_NewTDTTOT(p_tdt, 0,0,0,0,0,0);
+    dvbpsi_DecodeTDTSection(p_tdt, p_section);
+    p_tdt_tot_decoder->pf_callback(p_tdt_tot_decoder->p_cb_data, p_tdt);
+  }
+
+  if(p_section->i_table_id == 0x73)
+  {
+    dvbpsi_tdt_tot_t* p_tot;
+    dvbpsi_NewTDTTOT(p_tot, 0,0,0,0,0,0);
+    dvbpsi_DecodeTOTSection(p_tot, p_section);
+    p_tdt_tot_decoder->pf_callback(p_tdt_tot_decoder->p_cb_data, p_tot);
+  }
+
+  dvbpsi_ReleasePSISections(p_section);
+}
+
+
+/*****************************************************************************
+ * dvbpsi_DecodeTDTSection
+ *****************************************************************************
+ * TDT decoder.
+ *****************************************************************************/
+void dvbpsi_DecodeTDTSection(dvbpsi_tdt_tot_t* p_tdt,
+                             dvbpsi_psi_section_t* p_section)
+{
+    // Decode UTC Time
+    dvbpsi_DecodeMJDUTC(p_section->p_payload_start, p_tdt);
+    p_tdt->p_first_descriptor = NULL;
+}
+
+/*****************************************************************************
+ * dvbpsi_DecodeTOTSection
+ *****************************************************************************
+ * TOT decoder.
+ *****************************************************************************/
+void dvbpsi_DecodeTOTSection(dvbpsi_tdt_tot_t* p_tot,
+                             dvbpsi_psi_section_t* p_section)
+{
+    uint16_t i_length;
+    uint8_t *p_byte, *p_end;
+
+    // Decode UTC Time
+    p_byte = p_section->p_payload_start;
+    dvbpsi_DecodeMJDUTC(p_byte, p_tot);
+    p_byte += 5;
+    i_length = ((p_byte[0] & 0xf) << 8) | p_byte[1];
+    p_byte += 2;
+    p_end = p_byte + i_length;
+
+    if( p_end > p_section->p_payload_end ) return;
+
+    while(p_byte + 2 <= p_end)
+    {
+        uint8_t i_tag = p_byte[0];
+        uint8_t i_desc_length = p_byte[1];
+        if(i_desc_length + 2 <= p_end - p_byte)
+          dvbpsi_TOTAddDescriptor(p_tot, i_tag, i_desc_length, p_byte + 2);
+        p_byte += 2 + i_desc_length;
+    }
+}
+
+
+void dvbpsi_DecodeMJDUTC(char *p_mjdutc, dvbpsi_tdt_tot_t* p_table)
+{
+    #define BCD_CHAR_TO_INT(_bcd) (((_bcd >> 4) * 10) + (_bcd & 0x0f))
+
+    uint16_t i_mjd = (((uint16_t)p_mjdutc[0] << 8) | (uint16_t)p_mjdutc[1]);
+    /*
+        To find Y, M, D from MJD
+        Y' = int [ (MJD - 15 078,2) / 365,25 ]
+        M' = int { [ MJD - 14 956,1 - int (Y' × 365,25) ] / 30,6001 }
+        D = MJD - 14 956 - int (Y' × 365,25) - int (M' × 30,6001)
+        If M' = 14 or M' = 15, then K = 1; else K = 0
+        Y = Y' + K
+        M = M' - 1 - K × 12
+    */
+    int i_temp_y, i_temp_m, i_k = 0;
+    i_temp_y = (int) (((double)i_mjd - 15078.2)/ 365.25);
+    i_temp_m = (int) ((((double)i_mjd - 14956.1) - ((double)i_temp_y * 365.25)) / 30.6001);
+
+    if ((i_temp_m == 14) || (i_temp_m == 15))
+    {
+        i_k = 1;
+    }
+
+    p_table->i_year = i_temp_y + i_k + 1900;
+    p_table->i_month = (i_temp_m - 1) - (i_k * 12);
+    p_table->i_day = (((i_mjd - 14956) - (int)((double)i_temp_y * 365.25)) - (int)((double)i_temp_m * 30.6001));
+    p_table->i_hour = BCD_CHAR_TO_INT(p_mjdutc[2]);
+    p_table->i_minute = BCD_CHAR_TO_INT(p_mjdutc[3]);
+    p_table->i_second = BCD_CHAR_TO_INT(p_mjdutc[4]);
+}
+
+/*****************************************************************************
+ * dvbpsi_TOTAddDescriptor
+ *****************************************************************************
+ * Add a descriptor in the TOT description.
+ *****************************************************************************/
+dvbpsi_descriptor_t *dvbpsi_TOTAddDescriptor(
+                                               dvbpsi_tdt_tot_t *p_tot,
+                                               uint8_t i_tag, uint8_t i_length,
+                                               uint8_t *p_data)
+{
+  dvbpsi_descriptor_t * p_descriptor
+                        = dvbpsi_NewDescriptor(i_tag, i_length, p_data);
+
+  if(p_descriptor)
+  {
+    if(p_tot->p_first_descriptor == NULL)
+    {
+      p_tot->p_first_descriptor = p_descriptor;
+    }
+    else
+    {
+      dvbpsi_descriptor_t * p_last_descriptor = p_tot->p_first_descriptor;
+      while(p_last_descriptor->p_next != NULL)
+        p_last_descriptor = p_last_descriptor->p_next;
+      p_last_descriptor->p_next = p_descriptor;
+    }
+  }
+
+  return p_descriptor;
+}
