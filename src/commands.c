e63ceb9c1c14b0d8ba84ee7164491bbdc4cce71e
Adam Charrett <adam@dvbstreamer.org>
2007-03-10 13:16:01 +0000

    Add 'all' option to scan command.

--- src/commands.c
+++ src/commands.c
@@ -103,6 +103,7 @@ static void CommandHelp(int argc, char **argv);
 
 static int ParsePID(char *argument);
 
+static void ScanMultiplex(Multiplex_t *multiplex);
 static void PATCallback(dvbpsi_pat_t* newpat);
 static void PMTCallback(dvbpsi_pmt_t* newpmt);
 static void SDTCallback(dvbpsi_sdt_t* newsdt);
@@ -298,7 +299,8 @@ static Command_t coreCommands[] = {
                                       TRUE, 1,1,
                                       "Scan the specified multiplex for services.",
                                       "scan <multiplex>\n"
-                                      "Tunes to the specified multiplex and wait 5 seconds for PAT/PMT/SDT.",
+                                      "Tunes to the specified multiplex and wait 5 seconds for PAT/PMT/SDT."
+                                      " If multiplex is 'all' then all multiplexes will be scanned.",
                                       CommandScan
                                   },
                                   {
@@ -1304,65 +1306,106 @@ static void CommandHelp(int argc, char **argv)
 
 static void CommandScan(int argc, char **argv)
 {
-    int muxfreq = atoi(argv[0]);
+    
     Multiplex_t *multiplex;
+    char currservice[SERVICE_MAX_NAME_LEN];
 
     CheckAuthenticated();
-
-    multiplex = MultiplexFind(muxfreq);
-    if (multiplex)
+    if (CurrentService)
     {
-        struct timespec timeout;
-        char currservice[SERVICE_MAX_NAME_LEN];
-        bool patreceivedstate = FALSE;
-        bool allpmtsreceivedstate = FALSE;
-        bool sdtreceivedstate = FALSE;
-
-
-        if (CurrentService)
+        strcpy(currservice, CurrentService->name);
+    }
+    if (strcmp(argv[0], "all") == 0)
+    {
+        int count = MultiplexCount();
+        
+        Multiplex_t **multiplexes = calloc(count, sizeof(Multiplex_t *));
+        if (multiplexes)
         {
-            strcpy(currservice, CurrentService->name);
+            int i =0;
+            MultiplexEnumerator_t enumerator = MultiplexEnumeratorGet();
+            do
+            {
+                multiplex = MultiplexGetNext(enumerator);
+                if (multiplex)
+                {
+                    multiplexes[i] = multiplex;
+                    i ++;
+                }
+            }while(multiplex && ! ExitProgram);
+            MultiplexEnumeratorDestroy(enumerator);
+
+            for (i = 0; (i < count) && ! ExitProgram; i ++)
+            {
+                ScanMultiplex(multiplexes[i]);
+                /* Free'd by SetMultiplex */
+            }
         }
-        SetMultiplex(multiplex);
-
-        patreceived = FALSE;
-        sdtreceived = FALSE;
-        allpmtsreceived = FALSE;
-        pmtcount = 0;
-        pmtsreceived = NULL;
-        clock_gettime( CLOCK_REALTIME, &timeout);
-        timeout.tv_sec += 5;
-        scanning = TRUE;
-
-        pthread_mutex_lock(&scanningmutex);
-        pthread_cond_timedwait(&scanningcond, &scanningmutex, &timeout);
-        pthread_mutex_unlock(&scanningmutex);
-        CommandPrintf(" PAT received? %s\n", patreceived ? "YES":"NO");
-
-        pthread_mutex_lock(&scanningmutex);
-        pthread_cond_timedwait(&scanningcond, &scanningmutex, &timeout);
-        pthread_mutex_unlock(&scanningmutex);
-        CommandPrintf(" PMT received? %s\n", allpmtsreceived ? "YES":"NO");
-
-        pthread_mutex_lock(&scanningmutex);
-        pthread_cond_timedwait(&scanningcond, &scanningmutex, &timeout);
-        pthread_mutex_unlock(&scanningmutex);
-        CommandPrintf(" SDT received? %s\n", sdtreceived ? "YES":"NO");
-
-        scanning = FALSE;
-
-        if (pmtsreceived)
+    }
+    else
+    {
+        int muxfreq = atoi(argv[0]);
+        multiplex = MultiplexFind(muxfreq);
+        if (multiplex)
         {
-            free(pmtsreceived);
+            ScanMultiplex(multiplex);
+            /* Free'd by SetMultiplex */
         }
+    }
 
-        if (currservice)
-        {
-            SetCurrentService(currservice);
-        }
+    if (currservice)
+    {
+        SetCurrentService(currservice);
     }
 }
 /************************** Scan Callback Functions **************************/
+
+static void ScanMultiplex(Multiplex_t *multiplex)
+{
+    struct timespec timeout;
+    bool patreceivedstate = FALSE;
+    bool allpmtsreceivedstate = FALSE;
+    bool sdtreceivedstate = FALSE;
+    
+    CommandPrintf("Scanning %d\n", multiplex->freq);
+
+
+
+    SetMultiplex(multiplex);
+
+    patreceived = FALSE;
+    sdtreceived = FALSE;
+    allpmtsreceived = FALSE;
+    pmtcount = 0;
+    pmtsreceived = NULL;
+    clock_gettime( CLOCK_REALTIME, &timeout);
+    timeout.tv_sec += 5;
+    scanning = TRUE;
+
+    pthread_mutex_lock(&scanningmutex);
+    pthread_cond_timedwait(&scanningcond, &scanningmutex, &timeout);
+    pthread_mutex_unlock(&scanningmutex);
+    CommandPrintf(" PAT received? %s\n", patreceived ? "YES":"NO");
+
+    pthread_mutex_lock(&scanningmutex);
+    pthread_cond_timedwait(&scanningcond, &scanningmutex, &timeout);
+    pthread_mutex_unlock(&scanningmutex);
+    CommandPrintf(" PMT received? %s\n", allpmtsreceived ? "YES":"NO");
+
+    pthread_mutex_lock(&scanningmutex);
+    pthread_cond_timedwait(&scanningcond, &scanningmutex, &timeout);
+    pthread_mutex_unlock(&scanningmutex);
+    CommandPrintf(" SDT received? %s\n", sdtreceived ? "YES":"NO");
+
+    scanning = FALSE;
+
+    if (pmtsreceived)
+    {
+        free(pmtsreceived);
+    }
+
+
+}
 static void PATCallback(dvbpsi_pat_t* newpat)
 {
     if (scanning && !patreceived)
