a1ba46ef056115f7deda5fadc94b16a5d1bec180
Adam Charrett <adam@dvbstreamer.org>
2008-02-16 13:37:55 +0000

    Fix build break by adding #define _GNU_SOURCE to define asprintf.

--- src/events.c
+++ src/events.c
@@ -22,6 +22,7 @@ Event Management functions, for creating and firing events to listeners.
 */
 
 #include "config.h"
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
